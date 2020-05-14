08e4bc1120fd3579806ea8dbe6a889adb2d3597f
Sunny8747 <tjsdn2560@naver.com>
2020-05-14 20:21:46 +0900

    Percolation.java complete

---
+++ Union_find/Percolation.java
@@ -0,0 +1,135 @@
+package Union_find;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+//import edu.princeton.cs.algs4.QuickFindUF;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Percolation {
+    //create n x n grid, all blocked
+    // if you want to use Quick_find algorithm then just change it
+    //private QuickFindUF quf;
+    private WeightedQuickUnionUF wquf;
+
+    private final int GRID_SIZE, TOP_INDEX, BOTTOM_INDEX;
+    private boolean [] grid;
+
+    //loc : 1 ~ n^2
+    //grid[] : 0 ~ (n^2 + 1)
+    //grid[0] : over top
+    //grid[n^2 +1] : under bottom
+
+    //constructor
+    public Percolation(int n){
+        if(n <= 0){
+            throw new IllegalArgumentException();
+        }
+        this.wquf = new WeightedQuickUnionUF(n*n + 2);
+        this.grid = new boolean[n*n + 2]; //false: blocked, true: open
+        this.GRID_SIZE = n;
+        this.TOP_INDEX = 0;
+        this.BOTTOM_INDEX = n*n + 1;
+    }
+
+    //open the site(row, col) if blocked
+    public void open(int row, int col){
+        if(row > this.GRID_SIZE || col > this.GRID_SIZE || row <= 0 || col <= 0){
+            throw new IllegalArgumentException();
+        }
+        int loc = (col-1)* this.GRID_SIZE + row;
+        if(this.grid[loc] == false){
+            this.grid[loc] = true;
+            check_adjacent_isOpen(row, col);
+        }
+    }
+    //check adjacent grid if isOpen then union to origin node
+    public void check_adjacent_isOpen(int row, int col){
+        int loc = (col-1)* this.GRID_SIZE + row;
+        
+        //if first line and last line  'isOpen'  then  'union'  to TOP, BOTTOM  
+        if(col == 1){
+            this.wquf.union(loc,TOP_INDEX);
+        }
+        if(col == this.GRID_SIZE){
+            this.wquf.union(loc,BOTTOM_INDEX);
+        }
+
+        if(col != 1){//top line
+            if(isOpen(row, col-1) == true){//up is open
+                this.wquf.union(loc, (col-2)* this.GRID_SIZE + row);//union
+            }
+        }
+        if(row != this.GRID_SIZE){//right line
+            if(isOpen(row+1, col) == true){
+                this.wquf.union(loc, (col-1)* this.GRID_SIZE + row + 1);//union
+            }
+        }
+        if(col != this.GRID_SIZE){//bottom line
+            if(isOpen(row, col+1) == true){
+                this.wquf.union(loc, (col)* this.GRID_SIZE + row);//union
+            }
+        }
+        if(row != 1) {//left line
+            if(isOpen(row-1, col) == true){
+                this.wquf.union(loc, (col-1)* this.GRID_SIZE + row - 1);//union
+            }
+        }
+    }
+
+    //is the site(row, col) open?
+    public boolean isOpen(int row, int col){
+        if(row > this.GRID_SIZE || col > this.GRID_SIZE || row <= 0 || col <= 0){
+            throw new IllegalArgumentException();
+        }
+        int loc = (col-1)* this.GRID_SIZE + row;
+        return this.grid[loc];
+    }
+
+    //is the site(row, col) full?
+    public boolean isFull(int row, int col){
+        if(row > this.GRID_SIZE || col > this.GRID_SIZE || row <= 0 || col <= 0){
+            throw new IllegalArgumentException();
+        }
+        int loc = (col-1) * this.GRID_SIZE + row;
+        if(this.wquf.find(loc) == this.wquf.find(0)){
+            return true;
+        }
+        return false;
+    }
+    //return the number of open sites
+    public int numberOfOpenSites(){
+        int num_site = 0;
+        for(int i = 1; i< this.BOTTOM_INDEX; i++){
+            if(this.grid[i] == true){
+                num_site++;
+            }
+        }
+        return num_site;
+    }
+    //does the system percolate?
+    public boolean percolates(){
+        if(this.wquf.find(this.TOP_INDEX) == this.wquf.find(this.BOTTOM_INDEX)){
+            return true;
+        }
+        return false;
+    }
+    //test client(optional)
+    public static void main(String[] args){
+        int rand_row, rand_col;
+        //int n = Integer.parseInt(args[0]);
+        int n = 10;
+        int count = 0;
+        Percolation pc = new Percolation(n);
+        while(pc.percolates() == false){
+            rand_row = StdRandom.uniform(1,n+1);
+            rand_col = StdRandom.uniform(1,n+1);
+            System.out.printf("%d :: row : %d col : %d\n",count, rand_row, rand_col);
+            pc.open(rand_row, rand_col);
+            count++;
+        }
+        System.out.printf("parants of top : %d\n",pc.wquf.find(pc.TOP_INDEX));
+        System.out.printf("parants of bottom : %d\n",pc.wquf.find(pc.BOTTOM_INDEX));
+    }
+}
--- Union_find/summary.md
+++ Union_find/summary.md
@@ -1,14 +1,68 @@
-#union_find
+Union_find
+===========
 
-1. Quick Find - low efficiency
+# Quick Find
+Tag : flat, find, 
+Defect :
+    Union too expensive
+    Trees are flat, but too expensive to keep them flat
 Init - N
 Union - N
 Find - 1
 
-2. Quick Union - more efficiency
-Tag : tree, graph, find node
+# Quick Union
+Tag : tree, forest, graph, find node
+Defect :
+    Trees can get tall
+    Find too expensive
 lazy approach
+Init - N
+Union - N * includes cost of finding roots
+find - N *worst case
+
+# Quick Union Improved
+- Weighting : linking root of smaller tree to root of larger tree (height or rank)
+Init - M
+Union - lg N = ln N + log_10 N
+find - lg N = ln N + log_10 N
+
+- Path compression : make samll tree bring to up
+- QU + path compression = N + M log N
+- weighted QU + path compression = N + M lg N
+
+# Application
+- Percolation
+- Dynamics connectivity
+- Equivanlence of finite state automata
+- Least common ancestor
+- Matlab's bwlabel() in image processing
+- Kruskal's minimum spanning tree algorithm
+
+<hr/>
+
+# Interview Question
+1) Social network connectivity. Given a social network containing nn members and a log file containing mm timestamps at which times pairs of members formed friendships, design an algorithm to determine the earliest time at which all members are connected (i.e., every member is a friend of a friend of a friend ... of a friend). Assume that the log file is sorted by timestamp and that friendship is an equivalence relation. The running time of your algorithm should be m log n or better and use extra space proportional to n.
+Note: these interview questions are ungraded and purely for your own enrichment. To get a hint, submit a solution.
+answer :
+- m number of union.
+- all members have to connect
+- the earliest time
+-> Weighted QU or weighted QU + path-compression
+
+2) Union-find with specific canonical element. Add a method \mathtt{find()}find() to the union-find data type so that \mathtt{find(i)}find(i) returns the largest element in the connected component containing ii. The operations, \mathtt{union()}union(), \mathtt{connected()}connected(), and \mathtt{find()}find() should all take logarithmic time or better.
+For example, if one of the connected components is {1, 2, 6, 9}{1,2,6,9}, then the \mathtt{find()}find() method should return 99 for each of the four elements in the connected components.
+answer : 
 
 
+3) Successor with delete. Given a set of nn integers S = \{ 0, 1, ... , n-1 \}S={0,1,...,n−1} and a sequence of requests of the following form:
+- Remove xx from SS
+- Find the successor of xx: the smallest yy in SS such that y \ge xy≥x.
+design a data type so that all operations (except construction) take logarithmic time or better in the worst case.
 
+answer : 
+
+<hr/>
+
+# Assignment#1
+- Percolation : JAVA
 
